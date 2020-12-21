a02e341d0e32d3bf4255a0d83daa4dd68d9ab155
Rick Ucker <rick.ucker@gmail.com>
2020-12-21 17:03:40 -0500

    Check if choco installed before installing it

--- Install-Requirements.ps1
+++ Install-Requirements.ps1
@@ -1,4 +1,7 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$choco = get-command -Name choco.exe -ErrorAction SilentlyContinue
+if ($choco -eq $Null) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 choco install -y poshgit dotnetcore-3.1-sdk
 
 Add-WindowsCapability -Online -Name NetFx3~~~~
