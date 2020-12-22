878b84cb5a5d1643a262d727065a66f6ec57dc25
Rick Ucker <rick.ucker@gmail.com>
2020-12-22 14:45:27 -0500

    Check if dirs exist before creating, run git by abs path

--- Setup-UnicornRepos.ps1
+++ Setup-UnicornRepos.ps1
@@ -1,6 +1,11 @@
-mkdir ~/repos
+if (-not (Test-Path ~/repos)) {
+    mkdir ~/repos
+}
 cd ~/repos
-git clone https://github.com/BabbageCom/unicorndvctestapp.git
+if (-not (Test-Path unicorndvctestapp)) {
+	# If git was just installed in the parent shell, it won't be found on $PATH yet
+	& 'C:\Program Files\Git\cmd\git.exe' clone https://github.com/BabbageCom/unicorndvctestapp.git
+}
 cd unicorndvctestapp
 Invoke-WebRequest https://download.babbage.com/access4u/unicorndvc_3.1.1.exe -UseBasicParsing -OutFile $env:TEMP/unicorndvc_3.1.1.exe
 Start-Process $env:TEMP\unicorndvc_3.1.1.exe -Wait
